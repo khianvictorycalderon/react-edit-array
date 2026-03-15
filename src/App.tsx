785ad88a8117d3a4dfd3ad48fd8135fda3c93b28
khianvictorycalderon <ckhianvictory@gmail.com>
2026-03-15 19:51:48 +0800

    Added mock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,31 @@
+interface MockDataProps {
+  id: number;
+  name: string;
+}
+
+const MOCK_DATA: MockDataProps[] = [
+  {
+    id: 712489,
+    name: "John Doe"
+  },
+  {
+    id: 162489,
+    name: "Jake Doe"
+  },
+  {
+    id: 247185,
+    name: "Blake Doe"
+  },
+  {
+    id: 375184,
+    name: "Jane Doe"
+  },
+  {
+    id: 812940,
+    name: "Alice Doe"
+  },
+];
+
 export default function App() {
   return (
     <div className="flex min-h-screen w-screen items-center justify-center bg-neutral-300 text-neutral-800">
